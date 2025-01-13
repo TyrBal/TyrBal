03833941de15ecdf91b00553b5237e6ea5d3d184
Tyron Ravn Svensli <158293126+TyronSvensli@users.noreply.github.com>
2025-01-13 13:04:27 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,23 +1,2 @@
 
-/*
-key§include id§first_saga.asm p§;
-
-key§define id§subtracting p§( id§number1 p§, id§number2 P§) p§{
-    key§return id§number1 op§- id§number2 p§;
-P§}
-
-id§greeting op§= lit§"heisann" p§;
-id§hei_pointer op§= lit§*greeting p§;
-
-id§liste op§= lit§[5] p§;
-id§liste[0] op§= lit§255 p§;
-
-key§for p§( id§i op§= lit§1 p§; id§i op§> lit§10 p§; lit§i op§++ p§) p§{
-    key§if p§( id§liste[i] op§!= lit§0 p§) p§{
-        id§subtracting p§( id§liste[i] P§, id§liste[i] p§) p§;
-    p§}
-p§}
-
-*/
-
 
