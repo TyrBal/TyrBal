1febc86756df43992896bb901c2b5d6d424dc72c
baldurht <baldurjupiter@gmail.com>
2025-01-14 10:16:39 +0100

    fixed so variables can have numbers in name

--- src/main.zig
+++ src/main.zig
@@ -76,7 +76,7 @@ const Lexer = struct {
 
     // scans an identifier or keyword
     fn identifier(self: *Lexer) Token {
-        while (std.ascii.isAlphabetic(self.lookAHead()) or self.lookAHead() == '_') {
+        while (std.ascii.isAlphanumeric(self.lookAHead()) or self.lookAHead() == '_') {
             _ = self.advance();
         }
         const text = self.source[self.start..self.current];
